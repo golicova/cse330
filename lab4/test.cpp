02c3df247507a0ebc67633d95a1d97774d6cf73e
golicova <golicova@users.noreply.github.com>
2017-12-06 10:52:38 -0800

    Create test.cpp

---
+++ lab4/test.cpp
@@ -0,0 +1,24 @@
+#include <iostream> 
+#include <cassert>
+#include <vector> 
+
+using namespace std;
+
+int main()
+{
+    vector<int> v;
+    vector<int> w;
+    
+    v.push_back(1);
+    w.push_back(2);
+
+    cout << v[0] << endl;
+    cout << w[0] << endl << endl;
+    
+    v=w;
+    
+    cout << v[0] << endl;
+    
+    
+    
+}
